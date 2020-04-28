8216aa4d789e44e55b58b3d4b2dfd2d045ac28c4
Ivan Holub <ivanholub.js@gmail.com>
2020-04-28 13:25:08 +0300

    feat: add basic slider

--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -5,3 +5,37 @@ import '../css/main.scss';
 import '../css/variables.scss';
 import '../css/footer.scss';
 import {cards, arrowRight , arrowLeft, submitButton, formInput, containerForCards} from './variables';
+
+
+
+
+
+
+// slider functions
+let currentItem = 0;
+
+
+
+
+arrowRight.addEventListener('click', function(){
+  const cardsArray = Array.from(cards);
+  if(currentItem < cardsArray.length){
+    cardsArray[currentItem].style.display = 'none';
+    currentItem+=1;
+  } else {
+      return false
+  }
+})
+
+arrowLeft.addEventListener('click', function(){
+    const cardsArray = Array.from(cards);
+    if(currentItem > 0){
+        cardsArray[currentItem-1].style.display = 'block';
+        currentItem-=1;
+    }
+  })
+
+
+
+
+
