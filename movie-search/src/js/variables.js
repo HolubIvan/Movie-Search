f113eb67400d1078c5f97ae6f223e7b4ba17e17e
Ivan Holub <ivanholub.js@gmail.com>
2020-04-28 12:51:13 +0300

    feat: add variables js file with variables

---
+++ movie-search/src/js/variables.js
@@ -0,0 +1,8 @@
+export {cards, arrowRight , arrowLeft, submitButton, formInput, containerForCards}; 
+
+const cards = document.querySelectorAll('.card');
+const arrowRight = document.querySelector('.arrow__right');
+const arrowLeft = document.querySelector('.arrow__left');
+const submitButton = document.querySelector('.form__button');
+const formInput = document.querySelector('.form__input');
+const containerForCards = document.querySelector('.container');
