f1bee246da38e29481fb81e4c4d29451456789ab
Ivan Holub <ivanholub.js@gmail.com>
2020-04-28 13:24:04 +0300

    feat: add js files for project

---
+++ movie-search/src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/style.css';
+import '../css/base.scss';
+import '../css/header.scss';
+import '../css/main.scss';
+import '../css/variables.scss';
+import '../css/footer.scss';
+import {cards, arrowRight , arrowLeft, submitButton, formInput, containerForCards} from './variables';
